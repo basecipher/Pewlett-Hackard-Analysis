6b6550fc224f8c495ce9621d27edc9c67dc87cd7
basecipher <70349112+basecipher@users.noreply.github.com>
2020-11-18 11:45:44 -0600

    testing

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,9 +1,23 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+SELECT 
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	t.title,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (t.to_date = '9999-01-01')
+ORDER BY e.emp_no;
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON emp_no, first_name, last_name, title,
+INTO unique_titles
+FROM retirement_titles
+ORDER BY (t.to_date = '9999-01-01') DESC;
